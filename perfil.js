95723c6523ffcdecfb0978adce2900648edc0262
Gustavo869 <162450568+Gustavo869@users.noreply.github.com>
2025-11-11 12:04:00 -0300

    Add files via upload

--- perfil.js
+++ perfil.js
@@ -1,10 +1,25 @@
-async function carregarPerfil() {
+async function carregarPerfil(){
     try {
         const token = localStorage.getItem("token");
-        if (!token){
-            alert("Realize Login!")
-            Windom.location.href ="login.html"
-            /
+        if (!token) {
+            alert("Realize login!")
+            windows.location.href = "login.html"
         }
+        const resposta = await fetch("http://127.0.0.1:3000/perfil", {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer" + token
+            }
+        })
+        const dados = await resposta.json()
+        if(dados.erro) {
+            alert("Erro: "+ dados.erro)
+            windows.location.href = "login.html"
+        }
+        document.getElementById("nome").value = dados.nome;
+        // terminar de mostrar os dados nos campos do HTML
+    } catch (erro) {
+        console.log(erro)
     }
 }
+document.addEventListener("DOMContentLoaded", carregarPerfil)
